4d58269afa62fa8c29adb9de9d414d0e4a60760d
Morris He <hexinmin@gmail.com>
2019-07-14 22:28:04 +0800

    add command line parameter for bufsize

--- os/gocp.go
+++ os/gocp.go
@@ -9,38 +9,49 @@ import (
 )
 
 const (
-	BufSize = 4096
+	Buf_4K = 4096 * (1<<iota)
+	Buf_8K
+	Buf_16K
+	Buf_32K
+	Buf_64K
 )
 
-var ip2 int
+var bs int
 func init() {
-	flag.IntVar(&ip2, "ip2", 98999999, "kskdskldsklds")
+	flag.IntVar(&bs, "bs", Buf_4K, "Copy Buf size")
 }
 
 func main() {
-	var ip = flag.Int("p1", 1234, "help message for flagname")
-
-
-
 	flag.Parse()
-	fmt.Println(*ip)
-	fmt.Println(ip2)
-	return
+	
+	var BufSize = Buf_4K
+	switch bs {
+	case 0:
+		BufSize = Buf_4K
+	case 1:
+		BufSize = Buf_8K
+	case 2:
+		BufSize = Buf_16K
+	case 3:
+		BufSize = Buf_32K
+	case 4:
+		BufSize = Buf_64K
+	default:
+		BufSize = Buf_4K
+	}
 
-
-
-	if len(os.Args[1:]) != 2 {
+	if len(flag.Args()) != 2 {
 		fmt.Printf("USAGE: %s src dst\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	fr, err := os.Open(os.Args[1])
+	fr, err := os.Open(flag.Arg(0))
 	if err != nil {
 		log.Fatal(err)
 	}	
 	defer fr.Close()
 
-	fw, err := os.Create(os.Args[2])
+	fw, err := os.Create(flag.Arg(1))
 	if err != nil {
 		log.Fatal(err)
 	}
