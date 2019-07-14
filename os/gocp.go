1b9d0359baa7199e4e6114192f9db345ada358a1
Morris He <hexinmin@gmail.com>
2019-07-14 18:39:40 +0800

    first go program for cp

--- os/gocp.go
+++ os/gocp.go
@@ -5,13 +5,30 @@ import (
 	"log"
 	"fmt"
 	"io"
+	"flag"
 )
 
 const (
 	BufSize = 4096
 )
 
+var ip2 int
+func init() {
+	flag.IntVar(&ip2, "ip2", 98999999, "kskdskldsklds")
+}
+
 func main() {
+	var ip = flag.Int("p1", 1234, "help message for flagname")
+
+
+
+	flag.Parse()
+	fmt.Println(*ip)
+	fmt.Println(ip2)
+	return
+
+
+
 	if len(os.Args[1:]) != 2 {
 		fmt.Printf("USAGE: %s src dst\n", os.Args[0])
 		os.Exit(1)
